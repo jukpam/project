10d3f4fdded9bd9d9d81a55c9fa5b602ad06756d
jukpam <phamphuongtruc111@gmail.com>
2024-05-16 21:07:07 +0700

    1st edit

--- src/componetns/header/Header.js
+++ src/componetns/header/Header.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import logo from "../../imgs/logo.png"
+import "./header.css"
 
 const Header = () => {
     return (
-        <div>
+        <div className="header">
+            <div>
+                <img src={logo} width="150px"/>
+            </div>
+
+            <div className="hd-main"> 
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
-            <Link to="/courses">Courses</Link>
+            <Link to="/courses">Courses <i class="fa-solid fa-chevron-down"></i></Link> 
+            
             <Link to="/books">Books</Link>
             <Link to="/test">Test</Link>
             <Link to="/store">Store</Link>
+            </div>
+
+            <div className="hd-right">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <button className="btn-n">Log in</button>
+            <button className="btn-s">Sign up</button>
+            </div>
         </div>
     );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Header from '../../componetns/header/Header';
+
 
 const Home = () => {
     return (
